a7ce42193b12b7c030eeecd352013fe8edd942fd
yxsy <56529455+yx-sy@users.noreply.github.com>
2025-06-11 21:02:42 +0700

    Add files via upload

---
+++ app/src/main/java/com/github/catvod/utils/imge.java
@@ -0,0 +1,9 @@
+package com.github.catvod.utils;
+
+public class Image {
+
+    public static final String FOLDER = "https://gitee.com/yx-sy/imgnp/raw/main/cus/folder.png";
+    public static final String VIDEO = "https://gitee.com/yx-sy/imgnp/raw/main/cus/video.png";
+    public static final String PUSH = "https://gitee.com/yx-sy/imgnp/raw/main/cus/push.png";
+
+}
